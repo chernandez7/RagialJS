0d706c64d12a2aa6a695de80f5680b160abe63d1
chernandez7 <chernandez7@luc.edu>
2016-11-22 19:22:13 -0600

    started logic of parsing item info

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 var express = require('express');
-var fs      = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-app.get('/scrape', function(req, res){})
+app.get('/scrape', function(req, res){
 
+  url = 'http://ragi.al/item/iRO-Renewal/hzE'; // Test URL
+
+  request(url, function(error, response, html) {
+    if(!error) {
+        var $ = cheerio.load(html);
+
+        // Initialize JSON object
+        var name, img_src;
+        var json = {
+          name : "",
+          img_src : "",
+        };
+
+        // Item Name
+        $('.mkt_left').filter(function() {
+            var data = $(this);
+            name = trimString(data.children().first().text());
+            console.log("title: " + name);
+            json.name = name;
+        })
+
+        // Image url
+        $('.itemdb').filter(function() {
+          var data = $(this);
+          img_src = trimString(data.children().first().attr('src'));
+          console.log("img_src: " + img_src);
+          json.img_src = img_src;
+        })
+
+    }
+
+  }); // End request
+}) // End app.get
+
+// Function to remove whitespace and escape code
+function trimString(str) {
+  return str.replace(/[\n\t\r]/g,"").trim();
+}
+
+// listening
 app.listen('8081')
-
-console.log('port 8081');
 
 exports = module.exports = app;
